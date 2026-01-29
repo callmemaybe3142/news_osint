a09846248868e45b09d25461a49f57e8d6cd8c09
Sine Pi <sinepi.dia@gmail.com>
2026-01-29 14:04:36 +0630

    favicon change

--- myanmar_conflit_news/app/sitemap.ts
+++ myanmar_conflit_news/app/sitemap.ts
@@ -13,7 +13,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: `${baseUrl}/#statistics`,
             lastModified: new Date(),
-            changeFrequency: "daily",
+            changeFrequency: "weekly",
             priority: 0.9,
         },
         {
